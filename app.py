7bd0c443b4237cb53ba1c0e0836fdaeb3d84715f
Luitpold Fäth <luitpold@Luitpolds-MBP.fritz.box>
2019-07-17 14:09:31 +0200

    first commit 3

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 # Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
-
+import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def main():
+    return "Welcome!"
+
+@app.route('/how are you')
+def hello():
+    return 'I am good, how about you?'
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(host="0.0.0.0", port=8080)
